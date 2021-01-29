19dd63944cfc6b58ee6a4ee352e5b07e1764513a
DylanCheetah <dylan_the_cheetah@outlook.com>
2021-01-29 01:26:11 -0500

    Added additional metadata to setup.

--- setup.py
+++ setup.py
@@ -1,3 +1,11 @@
 from setuptools import setup
 
-setup()
+#Load readme file
+with open("README.md", "r") as f:
+    long_desc = f.read()
+
+#Run setup
+setup(
+    long_description = long_desc,
+    long_description_content_type = "text/markdown"
+)
