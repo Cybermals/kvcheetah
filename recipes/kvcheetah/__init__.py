6def2244999b7bbae7a21246b1c0bff27ed948c4
DylanCheetah <dylan_the_cheetah@outlook.com>
2021-02-07 01:05:25 -0500

    Updated recipe version.

--- recipes/kvcheetah/__init__.py
+++ recipes/kvcheetah/__init__.py
@@ -10,7 +10,7 @@ class KvCheetah(CythonRecipe):
     # use their pre-written build processes
 
     name = "kvcheetah"
-    version = "1.3.1"
+    version = "1.4.0"
     url = "https://github.com/Cybermals/kvcheetah/archive/v{version}.tar.gz"
     # {version} will be replaced with self.version when downloading
 
